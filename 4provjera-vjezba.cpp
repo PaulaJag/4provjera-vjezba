4de02c190b4adbeb4f7c663aee08ade053bdcb93
PaulaJag <64426155+PaulaJag@users.noreply.github.com>
2020-05-03 22:42:27 +0200

    Zadatak 3.
    Pretraga prema prezimenu i imenu te se ispisuju brojevi računa i salda svih koji imaju isto prezime i ime ili se ispiše poruka da takvih računa nema

--- 4provjera-vjezba.cpp
+++ 4provjera-vjezba.cpp
@@ -22,6 +22,20 @@ bool tekuci(unsigned long long broj)
         return false;
 }
 
+bool ispis_podataka(string prezimeIme[], unsigned long long int brRacuna[],double saldo[],int brKlijenata,string pretrazivanje)
+{
+    int br=0;
+    for(int i=0;i<brKlijenata;i++)
+    {
+        if(prezimeIme[i]==pretrazivanje){
+        cout<<brRacuna[i]<<", "<<saldo[i]<<endl;
+        br++;}
+    }
+    if(br==0)
+        return false;
+    return true;
+}
+
 int main()
 {
 
@@ -76,6 +90,16 @@ int main()
             cout<<"Klijent koji ima najveci saldo: "<< prezimeIme[max_index]<<endl;
             cout<<"Broj racuna sa negativnim saldom: "<<count_if(saldo,saldo+brKlijenata,negativni_saldo)<<endl;
             cout<<"Broj tekucih racuna: "<<count_if(brRacuna,brRacuna+brKlijenata,tekuci)<<endl;
+            }
+
+            else if(izbor==3)
+            {
+            cout<< "Unesite ime i prezime koje pretrazujete: ";
+            string pretrazivanje;
+            cin.ignore();
+            getline(cin,pretrazivanje);
+            if(ispis_podataka(prezimeIme,brRacuna,saldo,brKlijenata,pretrazivanje)==false)
+                cout<<"Nema takvih klijenata"<<endl;
             }
         }
     return 0;
